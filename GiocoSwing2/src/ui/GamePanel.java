5134cb5d783b87b8260e21efe4e08d5b665024f9
Daniele Nicoli <daniele@nicoli.dev>
2026-03-18 13:04:34 +0100

    Aggiunta logica applicativa giocatore con relativo movimento

--- GiocoSwing2/src/ui/GamePanel.java
+++ GiocoSwing2/src/ui/GamePanel.java
@@ -1,20 +1,32 @@
 package ui;
 
+import model.Giocatore;
 import model.Pallino;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements KeyListener {
     int limiti;
+    int punteggio;
+    Giocatore giocatore;
     ArrayList<Pallino> pallini;
-    public static final int NUMERO_PALLINI = 3;
+    public static final int NUMERO_PALLINI = 10;
 
     public GamePanel(int limiti) {
+        this.punteggio = 0;
         this.limiti = limiti;
+        this.giocatore = new Giocatore();
         this.pallini = new ArrayList<>();
+
+        setFocusable(true);
+        addKeyListener(this);
+
         generaPallini();
     }
 
@@ -39,5 +51,37 @@ public class GamePanel extends JPanel {
                 g.fillOval(pallino.getX(), pallino.getY(), Pallino.SIZE, Pallino.SIZE);
             }
         }
+
+        // Disegno del giocatore
+        g.setColor(Color.BLUE);
+        g.fillRect(giocatore.getX(), giocatore.getY(), Giocatore.SIZE, Giocatore.SIZE);
+
+        // Punteggio
+        g.setColor(Color.BLACK);
+        g.drawString("Punti: " + punteggio, 10, 20);
     }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+            giocatore.setX(-10, getWidth());
+        }
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            giocatore.setX(10, getWidth());
+        }
+        if(e.getKeyCode() == KeyEvent.VK_UP) {
+            giocatore.setY(-10, getWidth());
+        }
+        if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+            giocatore.setY(10, getWidth());
+        }
+
+        repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
 }
